e1717150816d4bab89438dea9ceae2f114a01868
Cartor Chen <cartor.chen@splashtop.com>
2024-04-11 11:38:52 +0800

    update vlc-3.0.20-universal

--- vlc-3.0.20-universal.rb
+++ vlc-3.0.20-universal.rb
@@ -1,7 +1,7 @@
 # Documentation: https://docs.brew.sh/Formula-Cookbook
 #                https://rubydoc.brew.sh/Formula
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
-class Vlc3020Universal < Formula
+class vlc < Formula
   desc "Splashtop test vlc"
   homepage ""
   url "https://free.nchc.org.tw/vlc/vlc/3.0.20/macosx/vlc-3.0.20-universal.dmg"
