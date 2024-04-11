0d55383fa7fcd880a333d4d0145d28d4ca7d3578
Cartor Chen <cartor.chen@splashtop.com>
2024-04-11 11:31:24 +0800

    vlc-3.0.20-universal

---
+++ vlc-3.0.20-universal.rb
@@ -0,0 +1,32 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Vlc3020Universal < Formula
+  desc "Splashtop test vlc"
+  homepage ""
+  url "https://free.nchc.org.tw/vlc/vlc/3.0.20/macosx/vlc-3.0.20-universal.dmg"
+  sha256 "22a18f396ccc9876c60d5fb443116c96fd7d042d89d59e50cdcba36bf39dd54b"
+  license ""
+
+  # depends_on "cmake" => :build
+
+  def install
+    # Remove unrecognized options if they cause configure to fail
+    # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
+    system "./configure", "--disable-silent-rules", *std_configure_args
+    # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test vlc-3.0.20-universal`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
